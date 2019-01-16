2795b4126a5e3d9770301842918e164c8a19ea0c
Minh Tran <hoangminhftu2@gmail.com>
2019-01-16 10:54:58 +0700

    Add local dynamodb

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 # Serverless example
 
 ## Usage
+* `npm run install-local-dynamodb`
+  * Install local DynamoDB
+
 * `npm start`
   * Start local development with serverless-offline
 
-* `npm deploy`
+* `npm run deploy`
   * Deploy to AWS
   * Note: must install aws-cli to create local credentials beforehand
--- index.js
+++ index.js
@@ -6,7 +6,21 @@ const AWS = require('aws-sdk');
 
 
 const USERS_TABLE = process.env.USERS_TABLE;
-const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+const IS_OFFLINE = process.env.IS_OFFLINE;
+let dynamoDb;
+if (IS_OFFLINE === 'true') {
+  dynamoDb = new AWS.DynamoDB.DocumentClient({
+    region: 'localhost',
+    endpoint: 'http://localhost:8000',
+    accessKeyId: 'DEFAULT_ACCESS_KEY',  // needed if you don't have aws credentials at all in env
+    secretAccessKey: 'DEFAULT_SECRET'
+  })
+  console.log(dynamoDb);
+} else {
+  dynamoDb = new AWS.DynamoDB.DocumentClient();
+};
+
 
 app.use(bodyParser.json({ strict: false }));
 
